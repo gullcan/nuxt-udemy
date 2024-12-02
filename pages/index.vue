8a68941a84add68352c130fd0ebd99553956f1cc
gülcan <dgulcan88@gmail.com>
2024-12-02 23:08:36 +0300

    7.commit.SkillsHub eklendi.

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,18 @@
     <div>
       <!-- Carousel Bileşeni -->
       <Carousel />
-      
-      <!-- Başlık ve Açıklama -->
-      <div >
+  <div >
         <h2 class="heading-serif-xxl">İhtiyacınız olan tüm yetkinlikler tek bir yerde</h2>
         <p class="heading-serif-xl">
           Kritik yetkinliklerden teknik konulara kadar çeşitli alanları kapsayan Udemy, profesyonel gelişiminizi destekler.
         </p>
       </div>
+     
+     
+      <SkillsHub />
+      
+      <!-- Başlık ve Açıklama -->
+    
      <TrendTopics />
       
   
@@ -18,6 +22,7 @@
   
   <script setup>
   import Carousel from '/components/Carousel.vue';
+  import SkillsHub from '/components/SkillsHub.vue';
   import TrendTopics from '/components/TrendTopics.vue';
   
  
